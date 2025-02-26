104244ec33ad9e5b7777f659859a75f8a56338b4
Norbert-Oleksy <oleksy.norbert@gmail.com>
2025-02-26 19:47:41 +0100

    added new module
    Added new basic module
    A WhacAMole module requier to play a game of Whac-A-Mole (what a surprise).
    Each hit is worth 10 points. To complete a module minigame, player must earn
    100 - easy, 150 - medium, 200 - hard points

---
+++ Assets/Scripts/GameScripts/ModulesScriptsFolder/WhacAMoleModule.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WhacAMoleModule : Module
+{
+    #region Fields
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private List<Button> _buttons;
+    [SerializeField] private float lifeTime = 0.5f;
+    #endregion
+
+    #region Variables
+    private int score;
+    private int targetScore;
+    #endregion
+
+    #region Logic
+    public void ButtonAction(Button clickedButton)
+    {
+        if(!clickedButton.interactable) return;
+        clickedButton.interactable = false;
+        score += 10;
+        UpdateScoreText();
+        CheckCompleteCondition();
+    }
+
+    private void UpdateScoreText() => _scoreText.text = score.ToString();
+
+    private void Initialization()
+    {
+        score = 0;
+        targetScore = 50 + 50 * DataManager.instance.difficulty;
+        UpdateScoreText();
+        StartCoroutine(ModuleLogic());
+    }
+
+    private void CheckCompleteCondition()
+    {
+        if(score < targetScore) return;
+        _scoreText.color = Color.green;
+
+        active = true;
+        Bomb.instance.CheckIfBombIsDefused();
+    }
+
+    private IEnumerator ModuleLogic()
+    {
+        while (!active)
+        {
+            StartCoroutine(PopUpTarget(_buttons.FindAll(b => !b.interactable)));
+            yield return new WaitForSeconds(lifeTime);
+            if(GameManager.instance.stage != GameManager.gamestage.Game) yield return null;
+        }
+        yield return null;
+    }
+
+    private IEnumerator PopUpTarget(List<Button> buttons)
+    {
+        if(buttons == null) yield return null;
+
+        Button target = buttons[Random.Range(0, buttons.Count)];
+        target.interactable = true;
+        yield return new WaitForSeconds(lifeTime);
+        target.interactable = false;
+        yield return null;
+    }
+    #endregion
+
+    #region Unity-API
+    private void Awake()
+    {
+        Initialization();
+    }
+    #endregion
+}
